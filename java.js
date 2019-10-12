7f9c9ce2d1e73a6ad719d306c18a5549edd68f71
Tara DeGuzman <tara.deguzman323@gmail.com>
2019-10-11 18:46:44 -0700

    added some java code

---
+++ java.js
@@ -0,0 +1,35 @@
+(function()
+{
+    var quizQuestions = [{
+        question: "What is an informal, natural language description of a software system?",
+        options: ["Use story", "Boundary Object", "Benefits", "Case Practice"],
+        answer: 0
+    }, {
+        question: "What do user stories facilitate?",
+        options: ["Criteria", "Techniques", "Story Map", "Sensemaking & Communicating"],
+        answer: 3
+    }, {
+        question: "Who visited the Chrysler C3 project in Detroit in 1998?",
+        options: ["Bill Gates", "Steve Jobs", "Alistair Cockburn", "Joe Fraser"],
+        answer: 2
+    }, {
+        question: "Which is the most common user story template?",
+        options: ["Jagabond", "Connextra", "ExConlo", "Newark"],
+        answer: 1
+    }, {
+        question: "User stories are prioritized by whom?",
+        options: ["the developer", "the customer", "the seller", "the marketer"],
+        answer: 1
+    }, {
+        question: "Who defined ACCEPTANCE CRITERIA as 'notes about what the story must do in order for the project ownder to accept it as complete.'?",
+        options: ["Mike Cohn", "Daniel Craig", "Roy Stewart", "Stewie Douglas"],
+        answer: 0
+    }, {
+        question: "Which process uses the 'Double Diamond' rule?",
+        options: ["SHEQC", "JAVA", "PYTHON", "NCIS"],
+        answer: 0
+    }, {
+        question:
+    }
+    }]
+}
